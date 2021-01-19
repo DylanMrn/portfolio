2fd60a956882ee7a07bf49c91f71ee0183b7352a
DylanMrn <dy.marion92@gmail.com>
2021-01-19 21:27:28 +0100

    [Dylan] first maj source code

---
+++ html/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>PORTFOLIO</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="../css/banner.css">
+        <link rel="stylesheet" href="../css/body.css">
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/about.css">
+        
+        <!-- JS -->
+
+        <!-- ICONS -->
+        <link rel="stylesheet" href="../icons/css/all.min.css">
+        
+    </head>
+    <body>
+        <div style="display: flex;">
+            <header id="menu" style='display: flex; flex-direction: column;'>
+                <div class="logo" style='background-color: green; flex: 1;'>
+                    gjkjhkfjg
+                </div>
+                <nav style="flex: 2;">
+                    <div class="menu">
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">CV</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="signature" style='background-color: red; flex: 1;'>
+                    <p style="text-align: center; margin-top: 40%;"> © Copyright 2021 D. MARION <p>
+                </div>
+            </header>
+            <section class="first_page">
+                <article>
+                    <div class="circle"> </div>
+                    <h1 style="font-weight: bold; text-align: center; font-family: 'Calibri', sans-serif; font-size: 50px;"> Dylan Marion </h1>
+                    <div class="social" style="text-align: center;">
+                        <a href="" class="github">
+                            <i class="fas fa-user"></i>
+                        </a>
+                        <a href="" class="linkedin">
+                            <i class="fas fa-user"></i>
+                        </a>
+                    </div>
+                    <div style="text-align: center">
+                        <button class="button button_hire"> Hire Me ! </button>
+                    </div>
+                    <div class="logo_scroll">
+                        <i> </i>
+                    <div>
+                </article>
+            </section>
+        </div>
+        <section class="about" style='background-color: yellow; width: 100%; height: 50vh;'>
+            <article style="padding-left: 18%; padding-top: 4%;"> 
+                <h1> About ME </h1>
+                <div style="display: flex; float: left; flex-flow: row wrap; min-width: 100%;">
+                    <div class="circle2" style=""> </div>
+                    <div class="bubble" style="">
+                        blabla
+                    </div>
+                </div>
+                <div class="facts">
+                    <div class="sucess1"></div>
+                </div>
+            </article>
+        </section>
+        <section>
+            <article class="resume" style='background-color: green; width: 100%; height: 50vh; display: flex; margin: 0 auto; flex-flow: row wrap;'>
+                <div class="experience card" style="width: 600px; background-color: red; margin: 1% auto;">
+                    aaaaa
+                </div>
+                <div class="card studies" style="width: 600px; background-color: blue; margin: 1% auto">
+                    bbbb
+                </div>
+            </article>
+        </section>
+        <section>
+            <article class="works" style='background-color: purple; width: 100%; height: 50vh; display: flex; flex-flow: row wrap; justify-content: center;'>
+                <div class="card2" style="margin: 1%"> </div>
+                <div class="card2" style="margin: 1%"> </div>
+                <div class="card2" style="margin: 1%;"> </div>
+                <div class="card2" style="margin: 1%;"> </div>
+            </article>
+        </section>
+        <section>
+            <article class="services" style='background-color: blue; width: 100%; display: flex; justify-content: center; flex-flow: row wrap;'>
+                <div style="background-color: yellow; width: 25%; display: flex; flex-flow: row wrap; height: 275px; border-radius: 20px; margin: 2% 10px"> </div>
+                <div style="background-color: yellow; width: 30%; display: flex; flex-flow: row wrap; height: 400px; border-radius: 20px; margin: 1% 0%"> </div>
+                <div style="background-color: yellow; width: 25%; display: flex; flex-flow: row wrap; height: 275px; border-radius: 20px; margin: 2% 10px"> </div>
+            </article>
+        </section>
+        <section>
+            <article class="blog" style='background-color: yellow; width: 100%; display: flow-root; justify-content: center;'>
+                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
+                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
+                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
+                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
+                <button class="button button_read_more" style="display: block; margin: 1% auto"> Voir plus ! </button>
+            </article>
+        </section>
+        <section>
+            <article class="contact" style='background-color: green; width: 100%; height: 50vh;'>
+                <h1> Contact US </h1>
+                <form action="" method="get" class="form-example">
+                <div class="form-example">
+                    <label for="name">Enter your nom: </label>
+                    <input type="text" name="name" id="name" required>
+                </div>
+                <div class="form-example">
+                    <label for="email">Enter your email: </label>
+                    <input type="email" name="email" id="email" required>
+                </div>
+                <div class="form-example">
+                    <button class="button button_read_more" style="display: block; margin: 1% auto"> Envoie ! </button>
+                </div>
+                </form>
+            </article>
+        </section>
+        <footer id='footer'>
+            <p style=""> © Dylan Marion 2021 <a href> condition général d'utilisation </a> </p>
+        </footer>
+    </body>
+</html>
