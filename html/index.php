f282182feb997bc4530d087ccb5fd69c0a187f44
DylanMrn <dy.marion92@gmail.com>
2021-01-24 19:29:13 +0100

    [Dylan] add new contents + graphical modifications

--- html/index.php
+++ html/index.php
@@ -21,13 +21,19 @@
             <nav style="">
                 <ul>
                     <li>
-                        <a href="#">Accueil</a>
+                        <a href="#" class="puce">
+                            <i class="fas fa-home">  </i>
+                        </a>
                     </li>
                     <li>
-                        <a href="#">Blog</a>
+                        <a href="#" class="puce">
+                            <i class="fas fa-file"> </i>
+                        </a>
                     </li>
                     <li>
-                        <a href="#">CV</a>
+                        <a href="#" class="puce">
+                            <i class="fas fa-comment"> </i>
+                        </a>
                     </li>
                 </ul>
             </nav>
@@ -53,8 +59,7 @@
         </section>
         <section class="about" style='background-color: yellow; width: 100%;'>
             <h1 style="padding-left: 18%"> About ME </h1>
-            <article style="padding-left: 18%; padding-top: 4%;"> 
-                
+            <article style="padding-left: 18%;">
                 <div style="display: flex; flex-flow: row wrap; width: 100%;">
                     <div class="circle2" style=""> </div>
                     <div class="bubble" style="margin: 1%; padding: 1% 1% 1% 1%; display: flex; flex-flow: row wrap;">
@@ -68,7 +73,7 @@
                             <p> Gestion de projet </p>
                             <progress value="70" max="100" style="width: 100%; height: 30px">70 %</progress>
                             <p> Développement WEB (HTML, CSS, JS) </p>
-                            <progress value="70" max="100" style="width: 100%; height: 30px"> 70 %</progress>
+                            <progress value="70" max="100" style="width: 100%; height: 30px">70 %</progress>
                             <p> Communication / Marketing </p>
                             <progress value="70" max="100" style="width: 100%; height: 30px">70 %</progress>
                         </div>
@@ -81,72 +86,128 @@
         </section>
         <section>
             <article class="resume" style='background-color: green; width: 100%; display: flex; margin: 0 auto; flex-flow: row wrap; padding-left: 18%'>
-                <div class="card experience" style="width: 850px; background-color: red; margin: 1% auto; padding: 1%">
-                    <div class="test"> Experience
+                <div class="card experience" style="width: 700px; background-color: red; margin: 1% auto; padding: 1%">
+                    <div class="test"> Experiences
                         <details>
-                        <summary> SIACI SAINT HONORE </summary>
+                            <summary> SIACI SAINT HONORE </summary>
+                            <p> Chef de projet Junior </p>
+                            <p> Septembre 2019 - Aout 2021 </p> <p> 2 ans </p>
+                            <p> Contrat en alternance </p>
                         </details>
                         <details>
-                        <summary> SIACI SAINT HONORE </summary>
+                            <summary> SIACI SAINT HONORE </summary>
+                            <p> Développeur Junior </p>
+                            <p> Septembre 2018 - Septembre 2019 </p> <p> 1 ans </p>
+                            <p> Contrat en alternance </p>
                         </details>
                         <details>
-                        <summary> SIACI SAINT HONORE </summary>
-                        </details>
-                        <details>
-                        <summary>  MONOPRIX </summary>
+                            <summary>  MONOPRIX </summary>
+                            <p> Employé de commerce </p>
+                            <p> Septembre 2018 - Septembre 2019 </p> <p> 1 ans </p>
+                            <p> CDI étudiant </p>
                         </details>    
                         <details>
-                        <summary> E.LECLERC </summary>
+                            <summary> E.LECLERC </summary>
+                            <p> Employé de commerce / Comis de boulangerie </p>
+                            <p> février 2017 - Septembre 2018 </p> <p> 6 mois </p>
+                            <p> CDI étudiant </p>
                         </details>
                         <details>
-                        <summary> SIACI SAINT HONORE </summary>
+                            <summary> SIACI SAINT HONORE </summary>
+                            <p> Développeur Junior </p>
+                            <p> Septembre 2017 - Septembre 2017 </p> <p> 2 mois </p>
+                            <p> Stage </p>
                         </details>
                         <details>
-                        <summary> MCDONALD </summary>
+                            <summary> MCDONALD </summary>
+                            <p> Employé polyvalent </p>
+                            <p> Septembre 2016 - Septembre 2016 </p> <p> 4 mois </p>
+                            <p> CDI étudiant </p>
                         </details>
                     </div>
                 </div>
-                <div class="card studies" style="width: 850px; background-color: blue; margin: 1% auto; padding: 1%">
-                    <p> Etude </p>
-                    <div> EPITECH </div>
-                    <div> IUT DE VELIZY </div>
-                    <div> JULES FERRY </div>
-                    <div> AGORA </div>
+                <div class="card studies" style="width: 700px; background-color: blue; margin: 1% auto; padding: 1%">
+                    <div class="test"> Etudes
+                        <details>
+                            <summary> EPITECH </summary>
+                            <p> Msc PRO (4ème & 5ème année) </p>
+                            <p> Septembre 2019 - Septembre 2021 </p> <p> 2 ans </p>
+                            <p> RNCP niveau 1 Développeur d'application web & mobile spé Big Data (secondaire IA) </p>
+                        </details>
+                        <details>
+                            <summary> IUT de Vélizy </summary>
+                            <p> Licence PRO (3ème année) </p>
+                            <p> Septembre 2018 - Septembre 2019 </p> <p> 1 ans </p>
+                            <p> Licence Professionnel METWEB </p>
+                        </details>
+                        <details>
+                            <summary> Lycée Jules Ferry </summary>
+                            <p> BTS (1ème & 2ème année) </p>
+                            <p> Septembre 2016 - Septembre 2018 </p> <p> 2 ans </p>
+                            <p> BTS SN-IR </p>
+                        </details>
+                        <details>
+                            <summary> Lycée AGORA </summary>
+                            <p> BAC STI2D </p>
+                            <p> Septembre 2014 - Septembre 2016 </p> <p> 2 ans </p>
+                            <p> Baccalauréat STI2D SIN </p>
+                        </details>
+                    </div>
                 </div>
             </article>
         </section>
         <section>
-            <article class="works" style='background-color: purple; width: 100%; display: flex; flex-flow: row wrap; justify-content: center;'>
-                <div class="card2" style="margin: 1%; height: 175px;"> aaa</div>
-                <div class="card2" style="margin: 1%; height: 175px;">a </div>
+            <article class="works" style='background-color: purple; width: 100%; display: flex; flex-flow: row wrap; justify-content: center; padding-left: 18%'>
+                <div class="card2" style="margin: 1%; height: 175px;"> Projet EPITECH </div>
+                <div class="card2" style="margin: 1%; height: 175px;"> Réalisation d'un site vitrine </div>
+                <div class="card2" style="margin: 1%; height: 175px;"> Création d'un site de soundbox avec interface d'administration </div>
             </article>
         </section>
         <section>
-            <article class="services" style='background-color: blue; width: 100%; display: flex; justify-content: center; flex-flow: row wrap;'>
-                <div style="background-color: yellow; width: 25%; height: 275px; border-radius: 20px; margin: 2% 10px"> 
+            <article class="services" style='background-color: blue; width: 100%; display: flex; justify-content: center; flex-flow: row wrap; padding-left: 18%;'>
+                <div style="background-color: yellow; width: 400px; height: 275px; border-radius: 20px; margin: 2% 10px"> 
                     
                 </div>
-                <div style="background-color: yellow; width: 30%; height: 400px; border-radius: 20px; margin: 1% 0%"> 
-                    <p style="text-align: center;"> Chef de projet </p>
+                <div style="background-color: yellow; width: 700px; height: 400px; border-radius: 20px; margin: 1% 0%"> 
+                    <p style="text-align: center;"> Gestion de projet </p>
                     <div>
-                        Animation de réunion & rédaction de CR de réunion
-                        Réalisation de documentation
-                        Mise en place et utilisation d'outil de gestion de projet (Attlasian, Excel, ...)
-                        Création et suivi de planning
+                        <ul>
+                            <li>Animation de réunion & rédaction de CR de réunion</li>
+                            <li>Réalisation de documentation</li>
+                            <li>Mise en place et utilisation d'outil de gestion de projet (Attlasian, Excel, ...)</li>
+                            <li>Création et suivi de planning</li>
+                        </ul>
                     </div>
                 </div>
-                <div style="background-color: yellow; width: 25%; height: 275px; border-radius: 20px; margin: 2% 10px"> </div>
+                <div style="background-color: yellow; width: 400px; height: 275px; border-radius: 20px; margin: 2% 10px"> 
+
+                </div>
             </article>
         </section>
         <section>
-            <article class="blog" style='background-color: yellow; width: 100%; display: flow-root; justify-content: center;'>
-                <a href="">
-                    <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
-                    
-                </a>
-                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
-                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
-                <div style="background-color: green; width: 60%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
+            <article class="blog" style='background-color: yellow; width: 100%; display: flow-root; justify-content: center; padding-left: 18%'>
+                <div style="background-color: green; width: 70%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto">
+                    <a href="blog/article1.php" style="width: 100%"> 
+                        <h1> Création du blog </h1>
+                        <input type="datetime-local" id="meeting-time" name="meeting-time" value="2021-01-24T19:30" disabled>
+                        <p> Présentation de mon site du PORTFOLIO et des articles du blog... </p>
+                    </a>
+                </div>
+                <div style="background-color: green; width: 70%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> 
+                    <a href="blog/article1.php" style="width: 100%"> 
+                        <h1> Mes musiques et mes séries préférées </h1>
+                        <input type="datetime-local" id="meeting-time" name="meeting-time" value="2021-01-24T19:30" disabled>
+                        <p> Découvrer mes musiques/artistes préférées mais aussi mes séries du moment et mes coups de coeur... </p>
+                    </a>
+                </div>
+                <div style="background-color: green; width: 70%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto">
+                <a href="blog/article1.php" style="width: 100%"> 
+                        <h1> Mes méthodes d'apprentissage </h1>
+                        <input type="datetime-local" id="meeting-time" name="meeting-time" value="2021-01-24T19:30" disabled>
+                        <p> Présentation des techniques de travail et de mes sites de veilles... </p>
+                    </a>
+                </div>
+                <div style="background-color: green; width: 70%; display: flex; flex-flow: row wrap; height: 100px; border-radius: 20px; margin: 1% auto"> </div>
                 <button class="button button_read_more" style="display: block; margin: 1% auto"> Voir plus ! </button>
             </article>
         </section>
@@ -156,16 +217,17 @@
                     <h1> Contact US </h1>
                     <form action="" method="get" class="form-example">
                         <div class="form-example">
-                            <input class="form" type="text" name="name" id="name" placeholder="Name" input required>
+                            <input class="form_info" type="text" name="name" id="name" placeholder="Name" required>
+                            <input class="form_info" type="email" name="email" id="email" placeholder="E-mail" required>
                         </div>
                         <div class="form-example">
-                            <input class="form" type="email" name="email" id="email" placeholder="E-mail" required>
+                            <input class="form" type="text" name="sujet" id="sujet" placeholder="Sujet" required>
                         </div>
                         <div class="form-example">
                             <textarea name="message" id="InputMessage" class="form" placeholder="Message" style="height: 100px; margin: 1% 0 1% 0;"></textarea>
                         </div>
                         <div class="form-example">
-                            <button class="button button_read_more" style="display: block; margin: 1% auto;"> Envoie ! </button>
+                            <button class="button button_read_more" style=""> Envoie ! </button>
                         </div>
                     </form>
                     </div>
